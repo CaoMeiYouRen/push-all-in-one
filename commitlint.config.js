b0fce388c84d1211e781c3ca7f625ebb766cd5cf
CaoMeiYouRen <996881204@qq.com>
2022-01-24 14:46:05 +0800

    ci: 更新 CI 配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,19 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', [
-            'feat',
-            'fix',
-            'docs',
-            'style',
-            'refactor',
-            'test',
-            'chore',
-            'revert',
-            'upgrade',
-            'build',
-            'perf',
-        ]],
         'subject-full-stop': [0, 'never'],
         'subject-case': [0, 'never'],
     },
